caa1b300bf3e107f87b426204fad64e800795af1
Sarah Frainer <sbfrainer@gmail.com>
2024-05-19 14:56:19 -0300

    ex-3

--- src/calculo_inss.js
+++ src/calculo_inss.js
@@ -3,16 +3,16 @@ function calcularInss (salarioBruto) {
     let inss = 0;
 
     if(salarioBruto <= 1412) {
-        inss = salarioBruto / 0.075;
+        inss = salarioBruto * 0.075;
     }
     else if(salarioBruto>1412 && salarioBruto<=2666.68) {
-        inss = salarioBruto / 0.09;
+        inss = salarioBruto * 0.09;
     }
     else if(salarioBruto>2666.68 && salarioBruto<=4000.03) {
-        inss = salarioBruto / 0.12;
+        inss = salarioBruto * 0.12;
     }
     else if(salarioBruto>4000.03 && salarioBruto<=7786.02) {
-        inss = salarioBruto / 0.14;
+        inss = salarioBruto * 0.14;
     } else {
         inss = 908.85;
     }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 const calcularInss = require("./calculo_inss");
+const calcularImposto = require("./calculo_imposto_renda");
 
 console.log(calcularInss(3000));
